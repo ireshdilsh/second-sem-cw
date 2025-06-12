a714dcd6d0db98aca428b9c734105820f2754b9d
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 21:02:54 +0530

    remove unused adminDao initialization and doPost method from AdminServlet

--- src/main/java/com/example/backend/controller/AdminServlet.java
+++ src/main/java/com/example/backend/controller/AdminServlet.java
@@ -10,25 +10,4 @@ import jakarta.servlet.http.HttpSession;
 @WebServlet("/api/v1/admin")
 public class AdminServlet extends HttpServlet{
 
-    private AdminDao adminDao;
-
-    public void init() throws jakarta.servlet.ServletException {
-        adminDao = new AdminDao();
-    };
-
-    protected void doPost(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse resp) throws jakarta.servlet.ServletException ,java.io.IOException {
-        HttpSession session = req.getSession(false);
-
-        String name = req.getParameter("name");
-        String email = req.getParameter("email");
-        String password = req.getParameter("password");
-
-        Admin admin = new Admin();
-        admin.setName(name);
-        admin.setEmail(email);
-        admin.setPassword(password);
-
-        adminDao.addAdmin(admin);
-        // session.setAttribute("admin", admin);
-    };
 }
