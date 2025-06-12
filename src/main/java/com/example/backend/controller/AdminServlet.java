ee2093e1ab1546d33afaff71222b70efd0537faf
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 23:14:44 +0530

    feat: implement addNewAdmin method in AdminModel and update AdminServlet to handle admin creation

--- src/main/java/com/example/backend/controller/AdminServlet.java
+++ src/main/java/com/example/backend/controller/AdminServlet.java
@@ -9,6 +9,8 @@ import org.apache.commons.dbcp2.BasicDataSource;
 
 import com.example.backend.dto.AdminDto;
 import com.example.backend.model.AdminModel;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
@@ -20,34 +22,44 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/api/v1/admin")
 public class AdminServlet extends HttpServlet {
 
-    private AdminModel
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        BasicDataSource dataSource = (BasicDataSource) getServletContext().getAttribute("dataSource");
-
-        try {
-            AdminDto admin = mapper.readValue(req.getInputStream(), AdminDto.class);
-
-            Connection connection = dataSource.getConnection();
-            PreparedStatement statement = connection.prepareStatement(
-                    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
-
-            statement.setString(1, admin.getName());
-            statement.setString(2, admin.getEmail());
-            statement.setString(3, admin.getPassword());
-
-            int rows = statement.executeUpdate();
-            resp.setContentType("application/json");
-            mapper.writeValue(resp.getWriter(), Map.of("Admin Save Success !", rows));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+    private AdminModel model;
 
     @Override
     public void init() throws ServletException {
-       
+        model = new AdminModel();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // try {
+        // AdminDto admin = mapper.readValue(req.getInputStream(), AdminDto.class);
+
+        // Connection connection = dataSource.getConnection();
+        // PreparedStatement statement = connection.prepareStatement(
+        // "INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+
+        // statement.setString(1, admin.getName());
+        // statement.setString(2, admin.getEmail());
+        // statement.setString(3, admin.getPassword());
+
+        // int rows = statement.executeUpdate();
+        // resp.setContentType("application/json");
+        // mapper.writeValue(resp.getWriter(), Map.of("Admin Save Success !", rows));
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            BasicDataSource dataSource = (BasicDataSource) getServletContext().getAttribute("dataSource");
+            AdminDto adminDto = mapper.readValue(req.getInputStream(), AdminDto.class);
+            model.addNewAdmin(adminDto.getName(), adminDto.getEmail(), adminDto.getPassword(), dataSource, resp, req);
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/example/backend/model/AdminModel.java
+++ src/main/java/com/example/backend/model/AdminModel.java
@@ -1,5 +1,43 @@
 package com.example.backend.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Map;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import com.example.backend.dto.AdminDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class AdminModel {
-    
+
+    public void addNewAdmin(String name, String email, String password, BasicDataSource dataSource,
+            HttpServletResponse resp, HttpServletRequest req) {
+
+        ObjectMapper mapper = new ObjectMapper();
+        // dataSource = (BasicDataSource)
+        // getServletContext().getAttribute("dataSource");
+
+        try {
+            // AdminDto admin = mapper.readValue(req.getInputStream(), AdminDto.class);
+
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, password);
+
+            int rows = statement.executeUpdate();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), Map.of("Admin Save Success !", rows));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
