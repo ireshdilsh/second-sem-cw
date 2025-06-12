0809ba9089ee454ba922147f56a6275b03c1cebc
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 22:47:59 +0530

    feat: add AdminDto class for handling admin data transfer

---
+++ src/main/java/com/example/backend/dto/AdminDto.java
@@ -0,0 +1,12 @@
+package com.example.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AdminDto {
+    private String name;
+    private String email;
+    private String password;
+}
