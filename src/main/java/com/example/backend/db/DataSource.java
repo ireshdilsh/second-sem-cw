9022c436fc4856f21b392010605d03070fec4932
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 22:39:05 +0530

    style: format code in DataSource class for improved readability

--- src/main/java/com/example/backend/db/DataSource.java
+++ src/main/java/com/example/backend/db/DataSource.java
@@ -8,30 +8,30 @@ import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 @WebListener
-public class DataSource implements ServletContextListener{
+public class DataSource implements ServletContextListener {
 
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-          try {
-            ServletContext context = sce.getServletContext();
-        BasicDataSource dataSource = (BasicDataSource) context.getAttribute("dataSource");
-        dataSource.close();
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
+  @Override
+  public void contextDestroyed(ServletContextEvent sce) {
+    try {
+      ServletContext context = sce.getServletContext();
+      BasicDataSource dataSource = (BasicDataSource) context.getAttribute("dataSource");
+      dataSource.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-         ServletContext context = sce.getServletContext();
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/sem2");
-        ds.setUsername("root");
-        ds.setPassword("1234");
-        ds.setInitialSize(50);
-        ds.setMaxTotal(100);
+  @Override
+  public void contextInitialized(ServletContextEvent sce) {
+    ServletContext context = sce.getServletContext();
+    BasicDataSource ds = new BasicDataSource();
+    ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    ds.setUrl("jdbc:mysql://localhost:3306/sem2");
+    ds.setUsername("root");
+    ds.setPassword("1234");
+    ds.setInitialSize(50);
+    ds.setMaxTotal(100);
 
-        context.setAttribute("dataSource", ds);
-    }
+    context.setAttribute("dataSource", ds);
+  }
 }
