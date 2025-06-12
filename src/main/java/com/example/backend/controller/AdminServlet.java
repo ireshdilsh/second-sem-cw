e444308e5e6573a8d9738036a1cc07836674233a
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 21:12:38 +0530

    add AdminServlet for admin creation and implement DataSource management

--- src/main/java/com/example/backend/controller/AdminServlet.java
+++ src/main/java/com/example/backend/controller/AdminServlet.java
@@ -1,7 +1,13 @@
 package com.example.backend.controller;
 
-import com.example.backend.dao.AdminDao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Map;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import com.example.backend.model.Admin;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -9,5 +15,25 @@ import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/api/v1/admin")
 public class AdminServlet extends HttpServlet{
+ protected void doPost(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse resp) throws jakarta.servlet.ServletException ,java.io.IOException {
+     ObjectMapper mapper = new ObjectMapper();
+        // Map<String, String> data = mapper.readValue(req.getInputStream(), Map.class);
+        BasicDataSource ds = (BasicDataSource) getServletContext().getAttribute("dataSource");
+        Admin data = new Admin();
+        try {
+            Connection connection = ds.getConnection();
+            PreparedStatement statement = connection
+                    .prepareStatement("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
 
+            statement.setString(1, data.getName());
+            statement.setString(2, data.getEmail());
+            statement.setString(3, data.getPassword());
+            // statement.setString(4, data.get("contact"));
+            int rows = statement.executeUpdate();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), rows);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ };
 }
--- src/main/java/com/example/backend/db/DataSource.java
+++ src/main/java/com/example/backend/db/DataSource.java
@@ -3,20 +3,28 @@ package com.example.backend.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
 
 @WebListener
 public class DataSource implements ServletContextListener{
-    public default void contextDestroyed(jakarta.servlet.ServletContextEvent sce) {
-        ServletContext context = sce.getServletContext();
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+          try {
+            ServletContext context = sce.getServletContext();
         BasicDataSource dataSource = (BasicDataSource) context.getAttribute("dataSource");
         dataSource.close();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
     }
 
-    public default void contextInitialized(jakarta.servlet.ServletContextEvent sce) {
-        ServletContext context = sce.getServletContext();
-        BasicDataSource dataSource = new BasicDataSource();
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+         ServletContext context = sce.getServletContext();
+        BasicDataSource ds = new BasicDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
         ds.setUrl("jdbc:mysql://localhost:3306/sem2");
         ds.setUsername("root");
@@ -24,6 +32,27 @@ public class DataSource implements ServletContextListener{
         ds.setInitialSize(50);
         ds.setMaxTotal(100);
 
-        context.setAttribute("dataSource", dataSource);
+        context.setAttribute("dataSource", ds);
     }
+
+    // @Override
+    // public default void contextDestroyed(jakarta.servlet.ServletContextEvent sce) {
+    //     ServletContext context = sce.getServletContext();
+    //     BasicDataSource dataSource = (BasicDataSource) context.getAttribute("dataSource");
+    //     dataSource.close();
+    // }
+
+    // @Override
+    // public default void contextInitialized(jakarta.servlet.ServletContextEvent sce) {
+    //     ServletContext context = sce.getServletContext();
+    //     BasicDataSource dataSource = new BasicDataSource();
+    //     ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    //     ds.setUrl("jdbc:mysql://localhost:3306/sem2");
+    //     ds.setUsername("root");
+    //     ds.setPassword("1234");
+    //     ds.setInitialSize(50);
+    //     ds.setMaxTotal(100);
+
+    //     context.setAttribute("dataSource", dataSource);
+    // }
 }
