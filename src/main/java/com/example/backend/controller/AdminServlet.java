4f0cfa3e68b8dac47a57ae6f6f20bdf2b136bdf6
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-12 20:08:43 +0530

    add AdminServlet class to handle admin API requests

---
+++ src/main/java/com/example/backend/controller/AdminServlet.java
@@ -0,0 +1,11 @@
+package com.example.backend.controller;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/api/v1/admin")
+public class AdminServlet extends HttpServlet{
+    protected void doPost(jakarta.servlet.http.HttpServletRequest req, jakarta.servlet.http.HttpServletResponse resp) throws jakarta.servlet.ServletException ,java.io.IOException {
+        
+    };
+}
