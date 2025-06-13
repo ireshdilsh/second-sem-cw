cd67479da73238534ec4ab80ce021001949edd96
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 09:52:25 +0530

    style: improve code formatting and consistency in signup.js

--- js/signup.js
+++ js/signup.js
@@ -1,24 +1,31 @@
-$('#userRegisterBtn').on('click',()=>{
+$('#userRegisterBtn').on('click', () => {
 
     const API_URL = ''
 
     const admin = {
-        name : $('#name').val(),
-        email : $('#email').val(),
-        password : $('#password').val()
+        name: $('#name').val(),
+        email: $('#email').val(),
+        password: $('#password').val()
     }
 
-   $.ajax({
-    url : API_URL,
-    method : 'POST',
-    contentType : 'application/json',
-    data : JSON.stringify(admin),
-    success : (res)=>{
-        console.log(res);
-        window.location.href = '../pages/signin.html'
-    },
-    error : (err)=>{
-        console.log(err);
-    }
-   })
+    $.ajax({
+        url: API_URL,
+        method: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(admin),
+        success: (res) => {
+            console.log(res);
+            fieldsCler()
+            window.location.href = '../pages/signin.html'
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    })
 })
+
+const fieldsCler = () => {
+    $('#name').val('')
+    $('#email').val('')
+    $('#password').val('')
+}
