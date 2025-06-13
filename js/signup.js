e0b899d325329089435ace14ea08b3e858370cd3
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 09:46:56 +0530

    create new user add method for api

--- js/signup.js
+++ js/signup.js
@@ -1,3 +1,23 @@
 $('#userRegisterBtn').on('click',()=>{
-    alert("Btn is Working")
+
+    const API_URL = ''
+
+    const admin = {
+        name : $('#name').val(),
+        email : $('#email').val(),
+        password : $('#password').val()
+    }
+
+   $.ajax({
+    url : API_URL,
+    method : 'POST',
+    contentType : 'application/json',
+    data : JSON.stringify(admin),
+    success : (res)=>{
+        console.log(res);
+    },
+    error : (err)=>{
+        console.log(err);
+    }
+   })
 })
