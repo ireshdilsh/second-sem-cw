6201887820a6dc4fe20790ae5a96c2b8d8ead3f4
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 23:11:22 +0530

    feat: add EmpSigninServlet and implement signin functionality in EmployeeModel

---
+++ src/main/java/com/example/backend/controller/EmpSigninServlet.java
@@ -0,0 +1,37 @@
+package com.example.backend.controller;
+
+import java.io.IOException;
+
+import com.example.backend.dto.EmployeeDto;
+import com.example.backend.model.EmployeeModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/api/v1/emp/signin")
+public class EmpSigninServlet extends HttpServlet{
+
+    private EmployeeModel model;
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       try {
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeDto dto = mapper.readValue(req.getInputStream(), EmployeeDto.class);
+        model.signin(dto.getEmail(), req, resp);
+        
+       } catch (Exception e) {
+        e.printStackTrace();
+       }
+    }
+
+    @Override
+    public void init() throws ServletException {
+        model = new EmployeeModel();
+    }
+
+}
--- src/main/java/com/example/backend/model/EmployeeModel.java
+++ src/main/java/com/example/backend/model/EmployeeModel.java
@@ -1,7 +1,9 @@
 package com.example.backend.model;
 
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Map;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -29,6 +31,40 @@ public class EmployeeModel {
             int rows = statement.executeUpdate();
             resp.setContentType("application/json");
             mapper.writeValue(resp.getWriter(), Map.of("Employee Save Success !", rows));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void signin(String email, HttpServletRequest req, HttpServletResponse resp) {
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT * FROM employee WHERE email = ?");
+
+            statement.setString(1, email);
+
+            ResultSet resultSet = statement.executeQuery();
+            resp.setContentType("application/json");
+            PrintWriter out = resp.getWriter();
+
+            if (resultSet.next()) {
+                resp.setStatus(HttpServletResponse.SC_OK);
+                mapper.writeValue(out, Map.of(
+                        "code", "200",
+                        "status", "Login Success",
+                        "message", "You have been logged in successfully"));
+            } else {
+                resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                mapper.writeValue(out, Map.of(
+                        "code", "401",
+                        "status", "Login Failed",
+                        "message", "You have been logged in failed"));
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
