65d493d06b114b10074e0f49a17af52682fa2d04
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 16:26:29 +0530

    feat: implement addNewComplain method in ComplainModel for complaint submission

--- src/main/java/com/example/backend/model/ComplainModel.java
+++ src/main/java/com/example/backend/model/ComplainModel.java
@@ -1,5 +1,13 @@
 package com.example.backend.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Map;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -7,8 +15,22 @@ public class ComplainModel {
 
     public void addNewComplain(String name, String email, String message, HttpServletResponse resp,
             HttpServletRequest req) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addNewComplain'");
+
+        ObjectMapper mapper = new ObjectMapper();
+        BasicDataSource dataSource = (BasicDataSource) req.getServletContext().getAttribute("dataSource");
+        try {
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO  complains (name, email, message) VALUES (?, ?, ?)");
+            statement.setString(1, name);
+            statement.setString(2, email);
+            statement.setString(3, message);
+            int rows = statement.executeUpdate();
+            resp.setContentType("application/json");
+            mapper.writeValue(resp.getWriter(), Map.of("Complain Save Success !", rows));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
