ee1d1b1650d67e99cb10ebe32c01e4c2ed04f0ba
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 16:22:05 +0530

    feat: implement doPost method in ComplaintServlet for handling complaints

--- src/main/java/com/example/backend/controller/ComplaintServlet.java
+++ src/main/java/com/example/backend/controller/ComplaintServlet.java
@@ -2,7 +2,9 @@ package com.example.backend.controller;
 
 import java.io.IOException;
 
+import com.example.backend.dto.ComplainDto;
 import com.example.backend.model.ComplainModel;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,28 +13,34 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/api/v1/complains")
-public class ComplaintServlet extends HttpServlet{
+public class ComplaintServlet extends HttpServlet {
 
     private ComplainModel model;
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       
+
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            ComplainDto dto = mapper.readValue(req.getInputStream(), ComplainDto.class);
+            model.addNewComplain(dto.getName(), dto.getEmail(), dto.getMessage(), resp, req);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void init() throws ServletException {
         model = new ComplainModel();
-    }   
+    }
 
 }
--- src/main/java/com/example/backend/model/ComplainModel.java
+++ src/main/java/com/example/backend/model/ComplainModel.java
@@ -1,5 +1,14 @@
 package com.example.backend.model;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class ComplainModel {
-    
+
+    public void addNewComplain(String name, String email, String message, HttpServletResponse resp,
+            HttpServletRequest req) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addNewComplain'");
+    }
+
 }
