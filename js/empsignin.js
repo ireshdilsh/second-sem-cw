78b32dc2061f84d23c96da48ae13343e7cf99a08
ireshdilsh <ireshdilshan46@gmail.com>
2025-06-13 23:16:42 +0530

    feat: store employee email in local storage upon signin and display it on dashboard

--- js/empsignin.js
+++ js/empsignin.js
@@ -1,8 +1,35 @@
+// $('#empsigninBtn').on('click', () => {
+//     const url = 'http://localhost:8080/backend/api/v1/emp/signin'
+//     const loginsEmp = {
+//         email: $('#email').val(),
+//     }
+
+//     $.ajax({
+//         url: url,
+//         method: 'POST',
+//         contentType: 'application/json',
+//         data: JSON.stringify(loginsEmp),
+//         success: (res) => {
+//             console.log(res);
+//             window.location.href = '../pages/empdash.html'
+//         },
+//         error: (err) => {
+//             Swal.fire({
+//                 icon: "error",
+//                 title: "Oops...",
+//                 text: "Something went wrong!"
+//             });
+//             console.log(err);
+//         }
+//     })
+
+// })
+
 $('#empsigninBtn').on('click', () => {
-    const url = 'http://localhost:8080/backend/api/v1/emp/signin'
+    const url = 'http://localhost:8080/backend/api/v1/emp/signin';
     const loginsEmp = {
         email: $('#email').val(),
-    }
+    };
 
     $.ajax({
         url: url,
@@ -11,7 +38,9 @@ $('#empsigninBtn').on('click', () => {
         data: JSON.stringify(loginsEmp),
         success: (res) => {
             console.log(res);
-            window.location.href = '../pages/empdash.html'
+
+            localStorage.setItem('empEmail', loginsEmp.email);
+            window.location.href = '../pages/empdash.html';
         },
         error: (err) => {
             Swal.fire({
@@ -21,6 +50,5 @@ $('#empsigninBtn').on('click', () => {
             });
             console.log(err);
         }
-    })
-
-})
+    });
+});
